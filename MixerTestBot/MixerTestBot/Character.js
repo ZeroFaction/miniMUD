08b5fe299b7fd6030baf1c589ba3c2a66e8f57b7
ZeroFaction <zerofactiongames@outlook.com>
2018-10-08 00:59:39 -0700

    Changes to allow character objects to be used in an array. This will open the door to allow for players to have unique characters with persistent stats eventually. Added a join command that creates a character on demand.

--- MixerTestBot/MixerTestBot/Character.js
+++ MixerTestBot/MixerTestBot/Character.js
@@ -1,52 +1,50 @@
-﻿function character(name, heroClass, level, location) {
-    this.setValue(name);
-    this.setValue(heroClass);
-    this.setValue(level);
-    this.setValue(location);
+﻿module.exports = class Character {
+    constructor(userN, charN, hClass, lvl, loc, cha, con, dex, inte, str, wis) {
+        this.userName = userN;
+        this.name = charN;
+        this.heroClass = hClass;
+        this.level = lvl;
+        this.location = loc;
+        this.charisma = cha;
+        this.constitution = con;
+        this.dexterity = dex;
+        this.intelligence = inte;
+        this.strength = str;
+        this.wisdom = wis;
+
+        // Setters
+        this.setName = function (newName) {
+            this.name = newName;
+        }
+        this.setLevel = function (newLevel) {
+            this.level = newLevel;
+        }
+        this.levelUp = function (earnedLevels) {
+            this.level += earnedLevels;
+            UpdateStats(this.level);
+        }
+        this.setLocation = function (newLocation) {
+            this.location = newLocation;
+        }
+
+
+        // Getters
+        this.getUsername = function () {
+            return this.userName;
+        }
+        this.getName = function () {
+            return this.name;
+        }
+        this.getLocation = function () {
+            return this.location;
+        }
+        this.getStats = function () {
+            return 'For user: ' + this.userName + '. ' + this.name + ' is a level ' + this.level + ' ' + this.heroClass + '. Currently at location ID: ' + this.location + '.';
+        }
+    }
 }
 
-// Set methods.
-character.method('setName', function (value) {
-    this.name = value;
-    return this;
-});
+function UpdateStats(statLevel) {
 
-character.method('setClass', function (value) {
-    this.heroClass = value;
-    return this;
-});
+}
 
-character.method('setLevel', function (value) {
-    this.level = value;
-    return this;
-});
-
-character.method('setLocation', function (value) {
-    this.location = value;
-    return this;
-});
-
-// Get methods.
-character.method('getName', function () {
-    return this.name;
-});
-
-character.method('getClass', function () {
-    return this.heroClass;
-});
-
-character.method('getLevel', function () {
-    return this.level;
-});
-
-character.method('getLocation', function () {
-    return this.location;
-});
-
-character.method('getStats', function () {
-    return '(' + this.getName() + '- Level ' + this.getLevel() + ' ' + this.getClass + '. Currently at location: ' + this.getLocation + ')';
-})
-
-//Test
-ladySelah = new character(Selah, Warrior, 1, 1);
-console.log(ladySelah.getStats());
